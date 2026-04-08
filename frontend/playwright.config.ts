d368a74c59c5ed10cb12e9f231e41c12d3752930
Selman Trabzon <selman.trbzon@gmail.com>
2026-04-08 11:44:24 +0300

    test: include images on tests too, since it's too fast

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   use: {
     baseURL: "http://127.0.0.1:4173",
     trace: "retain-on-failure",
-    screenshot: "only-on-failure",
+    screenshot: "on",
     launchOptions: {
       slowMo,
     },
