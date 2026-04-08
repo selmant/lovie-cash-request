037c6814c64760ae417dff40809af0e3ac19d22f
Selman Trabzon <selman.trbzon@gmail.com>
2026-04-08 11:52:38 +0300

    test: include traces too

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   ],
   use: {
     baseURL: "http://127.0.0.1:4173",
-    trace: "retain-on-failure",
+    trace: "on",
     screenshot: "on",
     launchOptions: {
       slowMo,
